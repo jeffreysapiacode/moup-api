d5961451b3f6ff6a27c682daa38c4b29ce3088bf
jeffreysapia <jeffsapia1988@gmail.com>
2026-04-20 22:16:22 -0400

    Added all necessary audio file information

--- src/main/java/io/moup/api/service/UploadService.java
+++ src/main/java/io/moup/api/service/UploadService.java
@@ -2,6 +2,7 @@ package io.moup.api.service;
 
 import io.moup.api.entity.Content;
 import io.moup.api.repository.ContentRepository;
+import io.moup.api.util.FileNameUtils;
 import io.moup.api.view.ContentView;
 import lombok.AllArgsConstructor;
 import org.apache.commons.io.FileUtils;
@@ -10,34 +11,39 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.Instant;
 
 @Service
 @AllArgsConstructor
 public class UploadService {
 
+    private static final String CONTENT_DIR = "./content/test.m4a";
+
     private final ContentRepository contentRepository;
 
     public ContentView uploadAndSave(String title, String description, MultipartFile file) {
-        IsoFile isoFile;
+        IsoFile audioFile;
         try {
-            FileUtils.copyInputStreamToFile(file.getInputStream(), new File("./content/test.m4a"));
-            isoFile = new IsoFile("./content/test.m4a");
-        } catch (Exception e) {
+            FileUtils.copyInputStreamToFile(file.getInputStream(), new File(CONTENT_DIR));
+            audioFile = new IsoFile(CONTENT_DIR);
+        } catch (IOException e) {
             throw new RuntimeException("Could not upload file: " + e.getMessage());
         }
         double lengthInSeconds = (double)
-                isoFile.getMovieBox().getMovieHeaderBox().getDuration() /
-                isoFile.getMovieBox().getMovieHeaderBox().getTimescale();
+                audioFile.getMovieBox().getMovieHeaderBox().getDuration() /
+                audioFile.getMovieBox().getMovieHeaderBox().getTimescale();
         Content content = contentRepository.save(Content.builder()
                 .title(title)
                 .description(description)
                 .duration(lengthInSeconds)
                 .uploadedOn(Instant.now())
+                .filename(FileNameUtils.formatFilename(title))
                 .build());
         return ContentView.builder()
                 .uuid(content.getUuid())
                 .title(content.getTitle())
+                .filename(content.getFilename())
                 .description(content.getDescription())
                 .duration(content.getDuration())
                 .uploadedOn(content.getUploadedOn())
--- src/main/java/io/moup/api/util/FileNameUtils.java
+++ src/main/java/io/moup/api/util/FileNameUtils.java
@@ -1,7 +1,11 @@
 package io.moup.api.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class FileNameUtils {
-    public static String formatFilename(String filename) {
-        return "Test";
+    public static String formatFilename(String title) {
+        String lowered = StringUtils.lowerCase(title);
+        String formatted0 = StringUtils.replaceChars(lowered, ",", "");
+        return StringUtils.replaceChars(formatted0, " ", "-");
     }
 }
