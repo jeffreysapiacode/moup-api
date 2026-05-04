b87d2896a53f4c51217a6a5415a088717a52254d
jeffreysapia <jeffsapia1988@gmail.com>
2026-05-04 00:53:34 -0400

    Final update

---
+++ src/main/java/io/moup/api/controller/AutoDictateController.java
@@ -0,0 +1,12 @@
+package io.moup.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auto-dictate")
+public class AutoDictateController {
+
+
+
+}
